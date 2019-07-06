ea7bd1f5ffcee7b65f30f97d4eb0520ed9c87d1d
Charles L Flatt <charles@tfhome.us>
2019-07-05 22:40:11 -0600

    Make New Story function like Edit Story

--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,2 @@
+nircmd elevate %systemroot%\system32\inetsrv\AppCmd.exe recycle apppool "MD User Stories"
 dotnet publish "source\MarkdownUserStories\MarkdownUserStories.csproj" /p:PublishProfile=LocalWebDeploy"
